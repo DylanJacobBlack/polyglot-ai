74476582ac9afe504a17ef0e2ce9da2718abe8f1
Dylan Black <dylan.jacob.black@gmail.com>
2023-03-26 15:48:32 -0500

    hide sign in button if logged in

--- src/components/NavBar/AuthenticateUser.tsx
+++ src/components/NavBar/AuthenticateUser.tsx
@@ -1,27 +1,24 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
-
 const AuthenicateUser: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
+  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined }
+  // );
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-8 py-2 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
+      {!sessionData && (
+        <button
+          className="rounded-full bg-white/10 px-8 py-2 font-semibold text-white no-underline transition hover:bg-white/20"
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 };
