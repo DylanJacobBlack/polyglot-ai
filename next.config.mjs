d0a74c59e949815f12eedce4ab3d98940f19d832
Dylan Black <dylan.jacob.black@gmail.com>
2023-05-13 20:21:44 -0500

    try different next.config.mjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
@@ -19,6 +19,13 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   },
 };
 export default config;
