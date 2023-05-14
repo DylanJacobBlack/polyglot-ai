efc1bdba36340d4dde989d567daeb7a7f3206a93
Dylan Black <dylan.jacob.black@gmail.com>
2023-05-14 07:49:50 -0500

    still trying to fix env variables for production

--- next.config.mjs
+++ next.config.mjs
@@ -18,12 +18,5 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL || "",
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID || "",
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || "",
-  },
 };
 export default config;
